832f4c1fd236690080833233572473f56c57fc32
guotie.9 <guotie.9@gmail.com>
2014-08-29 00:16:53 +0800

    modified:   main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/guotie/config"
@@ -10,6 +11,7 @@ import (
 	"github.com/smtc/justcms/utils"
 	"github.com/zenazn/goji"
 
+	"github.com/flosch/pongo2"
 	"github.com/smtc/justcms/admin"
 	_ "github.com/smtc/justcms/models"
 )
@@ -36,9 +38,41 @@ func run() {
 
 	goji.NotFound(utils.NotFound)
 
+	goji.Get("/", index)
+
 	goji.Serve()
 }
 
+// just for test
 func index(w http.ResponseWriter, r *http.Request) {
+	tpl := pongo2.Must(pongo2.FromFile("./themes/default/index.html"))
+	err := tpl.ExecuteWriter(pongo2.Context{
+		"title":     "JustCms demo",
+		"get_posts": get_posts,
+		"show_post": show_post,
+	}, w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
+func get_posts() []*models.Post {
+	return []*models.Post{
+		&models.Post{
+			Id:      1,
+			Title:   "a test post",
+			Content: "JustCms is written by golang, it's awesome",
+		},
+		&models.Post{
+			Id:      2,
+			Title:   "冷核聚变技术取得总大突破！！！",
+			Content: "历史性的一刻，人类再也不用为能源发愁了。",
+		},
+	}
+}
+
+func show_post(post *models.Post) string {
+	return fmt.Sprintf(`<h1>%s</h1>
+	<p>%s</p>
+	`, post.Title, post.Content)
 }
